e069056fe17626b9ff27cc1fdfcbed36ede51eec
FutureStyleGames <futurestylegames@gmail.com>
2021-04-25 12:35:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -121,41 +121,6 @@
         </div>
     </section>
 
-    <footer>
-        <div class="footer-inner">
-            <div class="footer-connect">
-                <p>Conntect with me</p>
-                <ul class="social-list">
-                    <a href="youtube.html">
-                        <img class="youtube-img" src="img/youtube-logo.png" alt="YouTube">
-                    </a>
-                    <a href="instagram.html">
-                        <img class="instagram-img" src="img/instagram-logo.png" alt="Instagram">
-                    </a>
-                    <a href="github.html">
-                        <img class="github-img" src="img/github-logo.png" alt="GitHub">
-                    </a>
-                    <a href="twitter.html">
-                        <img class="twitter-img" src="img/twitter-logo.png" alt="Twitter">
-                    </a>
-                </ul>
-            </div>
-    
-            <div class="footer-about">
-                <p>About</p>
-                <ul class="social-list">
-                    <a href="#">Who is Futureblur?</a>
-                    <a href="#">FAQ</a>
-                    <a href="#">Contact me</a>
-                </ul>
-            </div>
-    
-        </div>
-        <div class="footer-legal">
-            <a href="imprint.html">Imprint</a>
-            <a href="#">Privacy</a>
-            <p>&copy; Futureblur 2021</p>
-        </div>
-    </footer>
+    <?php include('footer.php'); ?>
 </body>
 </html>
